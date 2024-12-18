62cc822e3a2ce6306a15fea7e9daba8aff7ac310
PrimalPimmy <Prashant20.pm@gmail.com>
2024-12-18 15:32:35 +0530

    Control link change
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- core/cautils/controllink.go
+++ core/cautils/controllink.go
@@ -8,5 +8,5 @@ import (
 func GetControlLink(controlID string) string {
 	// For CIS Controls, cis-1.1.3 will be transformed to cis-1-1-3 in documentation link.
 	docLinkID := strings.ReplaceAll(controlID, ".", "-")
-	return fmt.Sprintf("https://hub.armosec.io/docs/%s", strings.ToLower(docLinkID))
+	return fmt.Sprintf("https://github.com/5GSEC/5G-Blueprint-Controls/blob/main/Controls/%s.md", strings.ToLower(docLinkID))
 }
